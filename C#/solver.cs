a144a403ffa577bb741ec7b46f41e6afbaaa32a7
Reihb <116902596+Reihb@users.noreply.github.com>
2022-11-22 22:24:54 +0100

    update

--- C#/solver.cs
+++ C#/solver.cs
@@ -12,7 +12,7 @@ namespace Sudoku_solver
 
             for(int a=0; a<Xtab.GetLength(0); a++)
             {
-                if(!res.Contains(Xtab[i,a]))
+                if((Xtab[i,a] != 0) && (!res.Contains(Xtab[i,a])))
                 {
                     res.Add(Xtab[i,a]);
                 }
@@ -20,7 +20,7 @@ namespace Sudoku_solver
 
             for(int a=0; a<Xtab.GetLength(1); a++)
             {
-                if(!res.Contains(Xtab[a,j]))
+                if((Xtab[i,a] != 0) && (!res.Contains(Xtab[a,j])))
                 {
                     res.Add(Xtab[a,j]);
                 }
